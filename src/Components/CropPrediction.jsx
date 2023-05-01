7084072ee643482f47d3903dcd1f290f44b4b5b7
MONSOON MAURYA <itssinghtite@gmail.com>
2023-05-01 13:25:21 +0530

    Update: crop prediction

--- src/Components/CropPrediction.jsx
+++ src/Components/CropPrediction.jsx
@@ -1,29 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
 
 const CropPrediction = () => {
-  return <Container>
-  {/* <div className="center-container">
-    hello peter
-  </div> */}
-  </Container>;
+  const [formData, setFormData] = useState({
+    nitrogen_content: "",
+    phosphorus_content: "",
+    potassium_content: "",
+    temperature: "",
+    humidity: "",
+    ph: "",
+    rainfall: "",
+  });
+
+  const [prediction, setPrediction] = useState("");
+  const [showResult, setShowResult] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: parseFloat(value) });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/predict",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setPrediction(response.data.crop);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container>
+      <div className="center-container">
+        <div className="heading">
+          <p>Crop Prediction</p>
+        </div>
+        <FormWrapper>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="nitrogen_content">Nitrogen content:</label>
+            <input
+              type="number"
+              id="nitrogen_content"
+              name="nitrogen_content"
+              value={formData.nitrogen_content}
+              onChange={handleChange}
+            />
+            <label htmlFor="phosphorus_content">Phosphorus content:</label>
+            <input
+              type="number"
+              id="phosphorus_content"
+              name="phosphorus_content"
+              value={formData.phosphorus_content}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="potassium_content">Potassium content:</label>
+            <input
+              type="number"
+              id="potassium_content"
+              name="potassium_content"
+              value={formData.potassium_content}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="temperature">Temperature:</label>
+            <input
+              type="number"
+              id="temperature"
+              name="temperature"
+              value={formData.temperature}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="humidity">Humidity:</label>
+            <input
+              type="number"
+              id="humidity"
+              name="humidity"
+              value={formData.humidity}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="ph">pH:</label>
+            <input
+              type="number"
+              id="ph"
+              name="ph"
+              value={formData.ph}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="rainfall">Rainfall:</label>
+            <input
+              type="number"
+              id="rainfall"
+              name="rainfall"
+              value={formData.rainfall}
+              onChange={handleChange}
+            />
+
+            <button type="submit">Predict Crop</button>
+          </form>
+
+          {prediction && (
+            <p
+              style={{
+                backgroundColor: "#4CAF50",
+                color: "white",
+                padding: "10px",
+                borderRadius: "5px",
+                marginTop: "20px",
+                textAlign: "center",
+                fontSize: "30px",
+                fontWeight: "bold",
+              }}
+            >
+              Recommended crop: {prediction}
+            </p>
+          )}
+        </FormWrapper>
+      </div>
+    </Container>
+  );
 };
 
 export default CropPrediction;
 
 const Container = styled.div`
-  display:flex;
   min-height: 100vh;
   height: auto;
   padding-top: 9.3rem;
   border-radius: 8px;
+  display: flex;
   justify-content: center;
   align-items: start;
+  background-color: #f8fff5;
   overflow-x: hidden;
-  height: 100vh;
-  width: 100%;
 
-  .center-container{
+  .center-container {
     display: flex;
     min-height: 85vh;
     flex-direction: column;
@@ -35,5 +159,81 @@ const Container = styled.div`
     padding-bottom: 2.5rem;
     width: 69%;
     gap: 2.5rem;
+    font-family: poppins;
+
+    .heading {
+      margin-top: 2.8rem;
+      width: 101rem;
+      height: 4.5rem;
+      border-bottom: 1px solid #bbb9b9ae;
+      display: flex;
+      justify-content: start;
+      align-items: center;
+      box-shadow: 0px 3px 6px -3px #d6d6d66b;
+
+      p {
+        margin: 0;
+        margin-bottom: 10px;
+        font-family: poppins;
+        font-size: 32px;
+        font-weight: 500;
+        color: darkgreen;
+      }
+    }
+  }
+`;
+
+const FormWrapper = styled.div`
+  flex: 1;
+  max-width: 500px;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  form {
+    // margin-top: 100px;
+  }
+  label {
+    display: block;
+    margin-bottom: 10px;
+    color: darkgreen;
+    font-weight: 500;
+  }
+
+  input {
+    width: 100%;
+    padding-left: 8px;
+    margin-bottom: 10px;
+    font-family: poppins;
+    border-radius: 3px;
+    border: 1px solid gray;
+    font-size: 12px;
+    height: 2.5rem;
+  }
+
+  button {
+    background-color: black;
+    color: white;
+    padding: 5px 8px;
+    border-radius: 3px;
+    border: none;
+    cursor: pointer;
+    margin-left: 150px;
+    font-size: 12px;
+    font-weight: 500;
+    transition: opacity 0.15s;
+
+    &:hvoer {
+      opacity: 0.9;
+    }
+
+    &:active {
+      opacity: 0.8;
+    }
   }
 `;
